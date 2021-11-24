a1e614895278e7fddb01db4f925f7dc125c492c6
Eddie318 <edward.moo@hotmail.com>
2021-11-24 21:10:15 +0800

    played more around it

--- ML/mlp.py
+++ ML/mlp.py
@@ -7,7 +7,37 @@ import math
 import datetime
 import platform
 
-print('Python version:', platform.python_version())
-print('Tensorflow version:', tf.__version__)
-print('Keras version:', tf.keras.__version__)
+#print('Python version:', platform.python_version())
+#print('Tensorflow version:', tf.__version__)
+#print('Keras version:', tf.keras.__version__)
 
+mnist_dataset = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist_dataset.load_data()
+
+#print('x_train:', x_train.shape)
+#print('y_train:', y_train.shape)
+#print('x_test:', x_test.shape)
+#print('y_test:', y_test.shape)
+
+#plt.imshow(x_train[0], cmap=plt.cm.binary)
+#plt.show()
+
+""" 
+numbers_to_display = 25
+num_cells = math.ceil(math.sqrt(numbers_to_display))
+plt.figure(figsize=(10,10))
+for i in range(numbers_to_display):
+    plt.subplot(num_cells, num_cells, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+    plt.xlabel(y_train[i])
+plt.show()
+"""
+
+x_train_normalized = x_train / 255
+x_test_normalized = x_test / 255
+
+#plt.imshow(x_train_normalized[0], cmap=plt.cm.binary)
+#plt.show()
